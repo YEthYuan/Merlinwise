c02dab4da15190e3f2c99a86097d1f2bdb388fb5
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-12 14:17:53 -0700

    update log specs

--- src/main/java/pro/yeyuan/merlinwisewiki/MerlinwiseWikiApplication.java
+++ src/main/java/pro/yeyuan/merlinwisewiki/MerlinwiseWikiApplication.java
@@ -1,13 +1,20 @@
 package pro.yeyuan.merlinwisewiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MerlinwiseWikiApplication {
 
+    private static final Logger LOG = (Logger) LoggerFactory.getLogger(MerlinwiseWikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MerlinwiseWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(MerlinwiseWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Successfully launch the app!!!");
+        LOG.info("Visit: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
