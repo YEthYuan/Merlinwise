d56268695f9b619f32463e72504d0c9f84af9aba
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-12 15:25:36 -0700

    add customized property

--- src/main/java/pro/yeyuan/merlinwisewiki/controller/TestController.java
+++ src/main/java/pro/yeyuan/merlinwisewiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package pro.yeyuan.merlinwisewiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testValue;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testValue;
     }
 
     @PostMapping("/hello/post")
