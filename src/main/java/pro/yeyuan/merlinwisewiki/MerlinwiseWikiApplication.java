6090efb64e476b615eb691285f0324af83a181ab
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-12 13:25:31 -0700

    Initial commit

---
+++ src/main/java/pro/yeyuan/merlinwisewiki/MerlinwiseWikiApplication.java
@@ -0,0 +1,13 @@
+package pro.yeyuan.merlinwisewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerlinwiseWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerlinwiseWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/yeyuan/merlinwisewiki/MerlinwiseWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.yeyuan.merlinwisewiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerlinwiseWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
