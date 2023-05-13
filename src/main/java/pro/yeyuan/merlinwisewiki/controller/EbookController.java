b46a9f597ad162f5477390c428c6e345c1904b5f
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-12 17:53:46 -0700

    add generic response class CommonResp, from then on every response type from backend controller should be CommonResp type

--- src/main/java/pro/yeyuan/merlinwisewiki/controller/EbookController.java
+++ src/main/java/pro/yeyuan/merlinwisewiki/controller/EbookController.java
@@ -3,6 +3,7 @@ package pro.yeyuan.merlinwisewiki.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pro.yeyuan.merlinwisewiki.domain.Ebook;
+import pro.yeyuan.merlinwisewiki.resp.CommonResp;
 import pro.yeyuan.merlinwisewiki.service.EbookService;
 
 import javax.annotation.Resource;
@@ -15,7 +16,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/ebook/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
