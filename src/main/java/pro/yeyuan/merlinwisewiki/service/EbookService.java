18f1c7bf381a378dce13436d60138b7e08735482
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-12 18:33:01 -0700

    Encapsulate BeanUtils with CopyUtil

--- src/main/java/pro/yeyuan/merlinwisewiki/service/EbookService.java
+++ src/main/java/pro/yeyuan/merlinwisewiki/service/EbookService.java
@@ -1,15 +1,14 @@
 package pro.yeyuan.merlinwisewiki.service;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import pro.yeyuan.merlinwisewiki.domain.Ebook;
 import pro.yeyuan.merlinwisewiki.domain.EbookExample;
 import pro.yeyuan.merlinwisewiki.mapper.EbookMapper;
 import pro.yeyuan.merlinwisewiki.req.EbookReq;
 import pro.yeyuan.merlinwisewiki.resp.EbookResp;
+import pro.yeyuan.merlinwisewiki.utils.CopyUtil;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,13 +24,15 @@ public class EbookService {
 
         List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> resp = new ArrayList<>();
-        for (Ebook ebook : ebooks) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            resp.add(ebookResp);
-        }
+//        List<EbookResp> resp = new ArrayList<>();
+//        for (Ebook ebook : ebooks) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+//            resp.add(ebookResp);
+//        }
 
-        return resp;
+        List<EbookResp> ebookResps = CopyUtil.copyList(ebooks, EbookResp.class);
+
+        return ebookResps;
     }
 }
