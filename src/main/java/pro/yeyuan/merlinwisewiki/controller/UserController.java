d1acfdcde5ee2f08d206eff12ac7a6df49b29915
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-14 11:52:48 -0700

    developed user table CRUD

---
+++ src/main/java/pro/yeyuan/merlinwisewiki/controller/UserController.java
@@ -0,0 +1,42 @@
+package pro.yeyuan.merlinwisewiki.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pro.yeyuan.merlinwisewiki.req.UserQueryReq;
+import pro.yeyuan.merlinwisewiki.req.UserSaveReq;
+import pro.yeyuan.merlinwisewiki.resp.CommonResp;
+import pro.yeyuan.merlinwisewiki.resp.UserQueryResp;
+import pro.yeyuan.merlinwisewiki.resp.PageResp;
+import pro.yeyuan.merlinwisewiki.service.UserService;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp<PageResp<UserQueryResp>> list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/pro/yeyuan/merlinwisewiki/service/UserService.java
@@ -0,0 +1,79 @@
+package pro.yeyuan.merlinwisewiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import pro.yeyuan.merlinwisewiki.domain.User;
+import pro.yeyuan.merlinwisewiki.domain.UserExample;
+import pro.yeyuan.merlinwisewiki.mapper.UserMapper;
+import pro.yeyuan.merlinwisewiki.req.UserQueryReq;
+import pro.yeyuan.merlinwisewiki.req.UserSaveReq;
+import pro.yeyuan.merlinwisewiki.resp.UserQueryResp;
+import pro.yeyuan.merlinwisewiki.resp.PageResp;
+import pro.yeyuan.merlinwisewiki.utils.CopyUtil;
+import pro.yeyuan.merlinwisewiki.utils.SnowFlake;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<UserQueryResp> list(UserQueryReq req) {
+
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+
+        if(!ObjectUtils.isEmpty(req.getLoginName())) {
+            criteria.andLoginNameEqualTo(req.getLoginName());
+        }
+
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<User> users = userMapper.selectByExample(userExample);
+
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+        LOG.info("总行数: {}", pageInfo.getTotal());
+        LOG.info("总页数: {}", pageInfo.getPages());
+
+//        List<UserQueryResp> resp = new ArrayList<>();
+//        for (User user : users) {
+//            UserQueryResp userResp = new UserQueryResp();
+//            BeanUtils.copyProperties(user, userResp);
+//            resp.add(userResp);
+//        }
+
+        List<UserQueryResp> userQueryResps = CopyUtil.copyList(users, UserQueryResp.class);
+        PageResp<UserQueryResp> resp = new PageResp<>();
+        resp.setTotal(pageInfo.getTotal());
+        resp.setList(userQueryResps);
+
+        return resp;
+    }
+
+    public void save(UserSaveReq req) {
+        User user = CopyUtil.copy(req, User.class);
+        if(ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            user.setId(snowFlake.nextId());
+            userMapper.insert(user);
+        } else {
+            // 更新
+            userMapper.updateByPrimaryKey(user);
+        }
+    }
+
+    public void delete(Long id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+}
