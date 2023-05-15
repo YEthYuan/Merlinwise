521da99b4dcf0362b968ad0500d9e3e75a2e1efe
YEthYuan <maxwell.yuanyeh@gmail.com>
2023-05-14 17:43:33 -0700

    prevent password modification

--- src/main/java/pro/yeyuan/merlinwisewiki/service/UserService.java
+++ src/main/java/pro/yeyuan/merlinwisewiki/service/UserService.java
@@ -79,6 +79,7 @@ public class UserService {
         } else {
             // 更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
